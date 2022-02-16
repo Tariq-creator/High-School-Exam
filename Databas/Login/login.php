234f7627652b5cb73f0afdeb586d764fa735973a
Tarik <91221056+Tariq-creator@users.noreply.github.com>
2022-02-16 10:43:17 +0100

    Update login.php

--- Databas/Login/login.php
+++ Databas/Login/login.php
@@ -2,14 +2,10 @@
 <html>
 <head>
     <meta charset="utf-8"/>
-    <title>Login</title>
-    <link rel="stylesheet" href="login.css"/>
-	<link href='https://unpkg.com/boxicons@2.0.9/css/boxicons.min.css' rel='stylesheet'>
+    <title>Logga In</title>
+    <link rel="stylesheet" href="login-style.css"/>
 </head>
 <body>
-
-<img src="login-classroom.png" width="100%">
-
 <?php
     require('db.php');
     session_start();
@@ -20,38 +16,38 @@
 		ucfirst($username);
         $password = stripslashes($_REQUEST['password']);
         $password = mysqli_real_escape_string($con, $password);
-        // Check user is exist in the database
+        // Check user exist in the database
         $query    = "SELECT * FROM `users` WHERE username='$username'
-                     AND password='" . md5($password) . "'";
+                     AND password='$password'";
         $result = mysqli_query($con, $query) or die(mysql_error());
         $rows = mysqli_num_rows($result);
         if ($rows == 1) {
             $_SESSION['username'] = $username;
-            // Redirect to user klassrum page
-            header("Location: klassrum.php");
+            // Redirect to user classrum page
+            header("Location: classrum.php");
         } else {
-            echo "<div class='form'>
-                  <h3>Incorrect Username/password.</h3><br/>
-                  <p class='link'>Click here to <a href='login.php'>Login</a> again.</p>
-                  </div>";
+            // Alert Wrong user and or password
+			header("Location: login.php");
         }
     } else {
 		
 ?>
+	<img class="background-img" src="background.jpg">
     <form method="post" name="login">
-	  <div class="grid">
-        <h1 class="login-title">Elevplacering</h1>
-	    <div class="user">
-	      <span>Användarnamn:</span><br/>
-          <input type="text" class="login-user" name="username" autofocus="true"/>
-	    </div>
-	    <div class="password">
-	      <span>Lösenord:</span><br/>
-	      <input type="password" class="login-password" name="password"/>
-	    </div>
-        <input type="submit" value="Logga in" name="submit" class="login-button"/>
-        <p class="link">Behöver du ett konto? <a href="registration.php">Klicka här</a></p>
+	  <div class="login-details">
+        <h1>Elevplacering</h1>
+	    <p>En platform för att underlätta lärarnas elevplacering!</p>
 	  </div>
+	  <div class="user">
+	    <span>Lärarens namn:</span><br/>
+        <input type="text" class="login-user" name="username" autofocus="true"/>
+	  </div>
+	  <div class="password">
+	    <span>Lösenord:</span><br/>
+	    <input type="password" class="login-password" name="password"/>
+	  </div>
+      <input type="submit" class="login-button" value="Logga in" name="submit"/>
+      <p class="link">Behöver du ett konto? <a href="registration.php">Klicka här</a></p>
     </form>
 <?php
     }
