30cb3dbe921b7d7b995883b04e0d3a37ec8008be
Tarik <91221056+Tariq-creator@users.noreply.github.com>
2022-02-23 11:14:11 +0100

    Update and rename db.php to createdb.php

---
+++ Databas/createdb.php
@@ -0,0 +1,46 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+// Create database
+$sql = "CREATE DATABASE firstTime";
+if ($conn->query($sql) === TRUE) {
+  echo "Database created successfully";
+} else {
+  echo "Error creating database: " . $conn->error;
+}
+
+$conn->close();
+
+$conn = new mysqli($servername, $username, $password, 'firstTime');
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+
+// sql to create table
+$sql = "CREATE TABLE users (
+ `id` int(11) NOT NULL AUTO_INCREMENT,
+ `username` varchar(50) NOT NULL,
+ `password` varchar(50) NOT NULL,
+ `create_datetime` datetime NOT NULL,
+ PRIMARY KEY (`id`)
+)";
+
+if ($conn->query($sql) === TRUE) {
+  echo "<br />Table users created successfully";
+} else {
+  echo "Error creating table: " . $conn->error;
+}
+
+$conn->close();
+?>
